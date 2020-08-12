c44973d8ce28fd1168ab78c92da60ed1deeed09d
Madison Werner <mwerner640@gmail.com>
2020-08-12 01:37:43 -0400

    One of the loops got messed up when I was fixing indentation errors.

--- MakinSnek.py
+++ MakinSnek.py
@@ -1,331 +1,259 @@
 import pygame
+
 import random
 from random import seed
 from random import randint
 
-pygame.init()
-key2 = 0
-W = 500
-H = 500
-win = pygame.display.set_mode((W,H))
+from enum import Enum
 
-pygame.display.set_caption("Snek :)")
+class Position:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
-white = (255, 255, 255)
-red = (255, 0, 0)
-green = (0, 255, 0)
+class Color():
+	def __init__(self):
+		self.WHITE = (255, 255, 255)
+		self.RED = (255, 0, 0)
+		self.GREEN = (0, 255, 0)
 
-font = pygame.font.Font('freesansbold.ttf', 8)
-font2 = pygame.font.Font('freesansbold.ttf', 20)
+class Game:
+	def __init__(self, display):
+		self.display = display
+		self.font1 = pygame.font.Font('freesansbold.ttf', 8)
+		self.font2 = pygame.font.Font('freesansbold.ttf', 20)
+		self.IN_PLAY = True
+		self.PAUSE = False
+		self.END_SUCCESS = False
+		self.position = Position(40,40)
+		self.food = Position(240,260)		
+		self.score = 0
+		self.prestige = 0
+		self.velocity = 20
+		self.rate = 17
+		self.length = 1
+		self.countcond = 10
+		self.level = 1
+		self.memlist = [[]]*103
+		self.colors = Color()
+	
+	def dispTEXT(self):
+		score_board = self.font1.render(f'SCORE: {self.score}  PRESTIGE: {self.prestige}', True, self.colors.WHITE)
+		score_board_rect = score_board.get_rect()
+		score_board_rect.center = (80, 8)
+		self.display.blit(score_board, score_board_rect)
 
+	def PauseScreen():
+		pause = self.font2.render("Press 'u' to unpause.", True, self.colors.WHITE)
+		pause_rect = pause.get_rect()
+		pause_rect.center = (250, 250)
+		self.display.blit(pause, pause_rect)
+		pygame.display.update()
+	
+	def winScreen(self):
+		congrats = self.font2.render('Congratulations, you beat the game!', True, self.colors.GREEN)
+		replay = self.font2.render("To play again, press 'space'. To quit, press 9.", True, self.colors.GREEN)
+		congrats_rect = congrats.get_rect()
+		replay_rect = replay.get_rect()
+		congrats_rect.center = (250 , 175)
+		replay_rect.center = (250 , 225)
+		self.display.blit(congrats, congrats_rect)
+		self.display.blit(replay, replay_rect)
 
-b = False
-x = 40
-y = 40
-width = 18
-height = 18
-vel = 20
-rate = 17
-length = 1
-dire = "R"
-diretemp = "R"
-count = -5
-countcond = 10
-score = 0
-run = True
-level = 1
-prestige = 0
-text = font.render(f'SCORE: {score}  PRESTIGE: {prestige}', True, white)
-text1win = font2.render('Congratulations, you beat the game!', True, green)
-text2win = font2.render("To play again, press 'space'. To quit, press 9.", True, green)
-text1lose = font2.render("You are as likely to be struck by lightning", True, red)
-text2lose = font2.render("as you are to achieve your dreams.", True, red)
-text3lose = font2.render("Try again with space, 9 to quit.", True, red)
-textPause = font2.render("Press 'u' to unpause.", True, white)
-textRect = text.get_rect()
-textw1 = text1win.get_rect()
-textw2 = text2win.get_rect()
-textl1 = text1lose.get_rect()
-textl2 = text2lose.get_rect()
-textl3 = text3lose.get_rect()
-textp = textPause.get_rect()
-textRect.center = (80, 8)
-textw1.center = (int(W/2) , int(H/2) - 75)
-textw2.center = (int(W/2) , int(H/2) + 75)
-textl1.center = (int(W/2) , int(H/2) - 100)
-textl2.center = (int(W/2) , int(H/2))
-textl3.center = (int(W/2) , int(H/2) + 100)
-textp.center = (int(W/2) , int(H/2))
+	def loseScreen(self):
+		lightning = self.font2.render("You are as likely to be struck by lightning", True, self.colors.RED)
+		dreams = self.font2.render("as you are to achieve your dreams.", True, self.colors.RED)
+		replay = self.font2.render("Try again with space, 9 to quit.", True, self.colors.RED)
+		lightning_rect = lightning.get_rect()
+		dreams_rect = dreams.get_rect()
+		replay_rect = replay.get_rect()
+		lightning_rect.center = (250 , 150)
+		dreams_rect.center = (250 , 250)
+		replay_rect.center = (250 , 350)
+		self.display.blit(lightning, lightning_rect)
+		self.display.blit(dreams, dreams_rect)
+		self.display.blit(replay,replay_rect)
 
-#To Update GameState:
-gamebool = True
-winbool = False
+	def wincond(self):
+		self.IN_PLAY = True
+		self.END_SUCCESS = False
 
-#count is used to make sure the square doesn't move every update. it would be way too quick.
-#dire is direction, diretemp accepts input from keyboard and updates dire upon refresh of screen.
+	def losecond(self):
+		self.END_SUCCESS = False
 
-memlist = [[]]*103
-memlist[0] = [x,y]
+	def eat(self):
+		if self.score == 100:
+			self.wincond()
+		self.length+=1
+		self.memlist[length-1] = [x , y]
+		self.food.x = randint(1,24) * 20
+		self.food.y = randint(1,24) * 20
+		while [self.food.x,self.food.y] in memlist:
+			self.food.x = randint(1,24) * 20
+			self.food.y = randint(1,24) * 20
+		self.score+=1
+		if self.score%10 == 0:
+			self.level+=1
+			self.rate-=1
+		if self.score % 20 == 0:
+			self.countcond-=1
 
-#this is for storing positions.
+	def move(self, dire):
+		if dire == "R":
+			if self.position.x >= 480:
+				self.losecond()
+			else:
+				self.position.x += self.velocity
+		elif dire == "L":
+			if self.position.x <= 0:
+            			self.losecond()
+			else:
+				self.position.x -= self.velocity
+		elif dire == "U":
+			if self.position.y <= 0:
+				self.losecond()
+			else:
+				self.position.y -= self.velocity
+		elif dire == "D":
+			if self.position.y >= 480:
+				self.losecond()
+			else:
+				self.position.y += self.velocity
 
-xf = 240
-yf = 260
+		for i in range(self.length):
+			if i == self.length - 1:
+				self.memlist[0] = [self.position.x,self.position.y]
+			else:
+				self.memlist[self.length - i - 1] = self.memlist[self.length - i - 2]
+		self.collcheck()
 
-#food position
-def gamestatereset():
-    global x
-    global y
-    global rate
-    global length
-    global dire
-    global diretemp
-    global count
-    global countcond
-    global score
-    global run
-    global level
-    global gamebool
-    global winbool
-    global memlist
-    global xf
-    global yf
-    global prestige
-    x = 40
-    y = 40
-    rate = 17
-    length = 1
-    dire = "R"
-    diretemp = "R"
-    count = -5
-    countcond = 10
-    score = 0
-    run = True
-    level = 1
-    gamebool = True
-    if winbool == True:
-        prestige = prestige + 1
-    else:
-        prestige = 0
-    winbool = False
-    memlist = [[]]*103
-    memlist[0] = [x,y]
-    xf = 240
-    yf = 260
-
-def wincond():
-    global winbool
-    global gamebool
-    winbool = True
-    gamebool = False
-
-def winScreen():
-    global text1win
-    global text2win
-    global textw1
-    global textw2
-    win.blit(text1win, textw1)
-    win.blit(text2win, textw2)
-
-def losecond():
-    global gamebool
-    gamebool = False
-
-def loseScreen():
-    global text1lose
-    global text2lose
-    global text3lose
-    global textl1
-    global textl2
-    global textl3
-    win.blit(text1lose, textl1)
-    win.blit(text2lose, textl2)
-    win.blit(text3lose,textl3)
-
-def eat():
-    global rate
-    global length
-    global memlist
-    global xf
-    global yf
-    global score
-    global level
-    global countcond
-    if score == 100:
-        wincond()
-    length = length + 1
-    memlist[length-1] = [x , y]
-    xf = randint(1,24) * 20
-    yf = randint(1,24) * 20
-    while [xf,yf] in memlist:
-        xf = randint(1,24) * 20
-        yf = randint(1,24) * 20
-    score = score + 1
-    if score % 10 == 0:
-        level = level + 1
-        rate = rate - 1
-    if score % 20 == 0:
-        countcond = countcond - 1
-
-def move():
-    global x, y
-    #global y
-    if dire == "R":
-        if x >= 480:
-            losecond()
-        else:
-            x += vel
-    if dire == "L":
-        if x <= 0:
-            losecond()
-        else:
-            x -= vel
-    if dire == "U":
-        if y <= 0:
-            losecond()
-        else:
-            y -= vel
-    if dire == "D":
-        if y >= 480:
-            losecond()
-        else:
-            y += vel
-
-    for i in range(length):
-        if i == length - 1:
-            memlist[0] = [x,y]
-        if i != length - 1:
-            memlist[length - i - 1] = memlist[length - i - 2]
-    collcheck()
+	def collcheck(self):
+		for i in range(self.length):
+			if self.memlist[0] == self.memlist[i] and i != 0:
+            			self.losecond()
 
 
-def collcheck():
-    global memlist
-    for i in range(length):
-        if memlist[0] == memlist[i] and i != 0:
-            losecond()
-            
-def dispTEXT():
-    global text
-    global score
-    text = font.render(f'SCORE: {score}  PRESTIGE: {prestige}', True, white)
-    win.blit(text,textRect)
 
-def PauseScreen():
-    global textPause
-    global textp
-    global b
-    global key2
-    win.blit(textPause,textp)
-    pygame.display.update()
-    key2 = pygame.key.get_pressed()
+def main():
+	pygame.init()
+	display = pygame.display.set_mode((500,500))
+	pygame.display.set_caption("Snek :)")
+	snek = Game(display)
+	
+	ACTIVE = True
 
+	snek.memlist[0] = [snek.position.x,snek.position.y]
+	count = -5
+	
+	while ACTIVE:
+		pygame.time.delay(snek.rate)
 
-while run:
-    pygame.time.delay(rate)
+		dire = "R"
+		diretemp = "R"
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				ACTIVE = False
+		keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_p]:
-        gamebool = False
-        b = True
-        PauseScreen()
-    
-    if keys[pygame.K_u]:
-        gamebool = True
-        b = False
+		if keys[pygame.K_p]:
+			snek.IN_PLAY = False
+			snek.PAUSE = True
+			snek.PauseScreen()
+	    
+		if keys[pygame.K_u]:
+			snek.IN_PLAY = True
+			snek.PAUSE = False
 
-    if b == True:
-        continue
-    
-    if gamebool == True:
-        if keys[pygame.K_LEFT]:
-            if dire == "R":
-                pass
-            else:
-                diretemp = "L"
-        if keys[pygame.K_RIGHT]:
-            if dire == "L":
-                pass
-            else:
-                diretemp = "R"
-        if keys[pygame.K_UP]:
-            if dire == "D":
-                pass
-            else:
-                diretemp = "U"
-        if keys[pygame.K_DOWN]:
-            if dire == "U":
-                pass
-            else:
-                diretemp = "D"
+		if snek.PAUSE == True:
+			continue
+	    
+		if snek.IN_PLAY == True:
+			if keys[pygame.K_LEFT]:
+				if dire == "R":
+					pass
+				else:
+					diretemp = "L"
+			if keys[pygame.K_RIGHT]:
+				if dire == "L":
+					pass
+				else:
+					diretemp = "R"
+			if keys[pygame.K_UP]:
+				if dire == "D":
+					pass
+				else:
+					diretemp = "U"
+			if keys[pygame.K_DOWN]:
+				if dire == "U":
+					pass
+			else:
+				diretemp = "D"
+	    
+			if  count <= snek.countcond:
+					count = count + 1
+			else:
+				if snek.memlist[0][0] == snek.food.x and snek.memlist[0][1] == snek.food.y:
+					snek.eat()
+				dire = diretemp
+				snek.move(dire)
+				count = 1
+		  
+			snek.display.fill((0,0,0))
+			snek.dispTEXT()
+			pygame.draw.rect(snek.display, (255,255,255), (snek.food.x + 4, snek.food.y + 4,10,10))
+		
+			for i in range(snek.length):
+				if i <= 10:
+					A = 255 - 10 * i
+					B = 0
+					C = 10 * i
+				elif i > 10 and i <= 20:
+					A = 155 - 5 * (i - 10)
+					B = 5 * (i - 10)
+					C = 100
+				elif i > 20 and i <= 30:
+					A = 105 + (i - 20) * 2
+					B = 50 + (i - 20) * 10
+					C = 100 + (i - 20) * 10
+				elif i > 30 and i <= 50:
+					A = 125 - (i - 30) * 5
+					B = 150 + (i - 30) * 5
+					C = 200 + (i - 30) * 1
+				elif i > 50 and i <= 70:
+					A = 24 + (i - 50) * 9
+					B = 244 - (i - 50) * 2
+					C = 224 - (i - 50)
+				else:
+					A = 204 + (i - 70)
+					B = A
+					C = A
+				pygame.draw.rect(snek.display, (A,B,C), (snek.memlist[i][0],snek.memlist[i][1], 18, 18))
+		    
+		elif snek.END_SUCCESS == True:
+			snek.winScreen()
+		else:
+			snek.loseScreen()
 
-    
-        if  count <= countcond:
-            count = count + 1
-        else:
-            if memlist[0][0] == xf and memlist[0][1] == yf:
-                eat()
-            dire = diretemp
-            move()
-            count = 1
-            
-        win.fill((0,0,0))
-        dispTEXT()
-        pygame.draw.rect(win, (255,255,255), (xf + 4, yf + 4,10,10))
-        
-        for i in range(length):
-            if i <= 10:
-                A = 255 - 10 * i
-                B = 0
-                C = 10 * i
-            if i > 10 and i <= 20:
-                A = 155 - 5 * (i - 10)
-                B = 5 * (i - 10)
-                C = 100
-            if i > 20 and i <= 30:
-                A = 105 + (i - 20) * 2
-                B = 50 + (i - 20) * 10
-                C = 100 + (i - 20) * 10
-            if i > 30 and i <= 50:
-                A = 125 - (i - 30) * 5
-                B = 150 + (i - 30) * 5
-                C = 200 + (i - 30) * 1
-            if i > 50 and i <= 70:
-                A = 24 + (i - 50) * 9
-                B = 244 - (i - 50) * 2
-                C = 224 - (i - 50)
-            if i > 70:
-                A = 204 + (i - 70)
-                B = A
-                C = A
-            pygame.draw.rect(win, (A,B,C), (memlist[i][0],memlist[i][1],width,height))
-            
-    elif winbool == True:
-        winScreen()
-    else:
-        loseScreen()
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_y]:
+			snek.losecond()
+		elif keys[pygame.K_z]:
+			snek.wincond()
+	    
+		pygame.display.update()
+		    
+		keys = pygame.key.get_pressed()
+		if snek.IN_PLAY == False:
+			if keys[pygame.K_9]:
+				quit()
+			if keys[pygame.K_SPACE]:
+				display = pygame.display.set_mode((500,500))
+				pygame.display.set_caption("Snek :)")
+				snek = game(display)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_y]:
-        losecond()
-    elif keys[pygame.K_z]:
-        wincond()
-    
-    pygame.display.update()
-            
-    keys = pygame.key.get_pressed()
-    if gamebool == False:
-        if keys[pygame.K_9]:
-            quit()
-        if keys[pygame.K_SPACE]:
-            gamestatereset()
-        
-        
+				snek.memlist[0] = [snek.position.x,snek.position.y]
+				count = -5
+	pygame.quit()
 
-    
-
-pygame.quit()
+main()
